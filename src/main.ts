14ee822058a9d4817099ff54389c2caefafa079f
mamaruxo <mamaruxo@users.noreply.github.com>
2024-01-26 00:27:50 -0500

    close to working!

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,206 @@
-import { join } from "path";
+import { join, basename } from "path";
 import { readFile, writeFile } from "fs/promises";
 
 import { fromHtml } from "hast-util-from-html";
 import { toMdast } from "hast-util-to-mdast";
 import { toMarkdown } from "mdast-util-to-markdown";
-import { visit } from "unist-util-visit";
+import { visit, SKIP } from "unist-util-visit";
 import { gfmFootnoteToMarkdown } from "mdast-util-gfm-footnote";
 
-import type { Element } from "hast";
-import type { FootnoteDefinition, FootnoteReference, Link, Root } from "mdast";
+import type { Element, Root as HastRoot } from "hast";
+import type {
+  FootnoteDefinition,
+  FootnoteReference,
+  Link,
+  Root,
+  Html,
+  Text,
+} from "mdast";
 
 const INPUT_DIR = "input";
 
-async function munge(filename: string) {
+const file1 = "part0014.html";
+const file2 = "part0070.html";
+
+const chap = await loadMungeConvert(file1);
+const footnotes = await loadMungeConvert(file2, true);
+
+// walk the main file, converting links to footnote references. link labels may
+// have duplicates but footnote identifiers have to be unique, so we track them
+// and rename as we go. for example, there may be several footnotes labelled
+// with an asterisk * in the original text -- we have to instead rename them to
+// **, ***, and so on.
+const usedIdentifiers = new Set<string>();
+const targetsToFinalIdentifiers = new Map<string, string>();
+visit(chap, "link", (node, index, parent) => {
+  if (!parent || typeof index !== "number") return;
+
+  const [file, target] = node.url.split("#");
+  if (file !== file2) {
+    throw new Error(`External link to unknown file: "${file}"`);
+  }
+  if (!target) throw new Error(`No target for link: "${node.url}"`);
+  if (node.children.length !== 1 && node.children[0].type !== "text") {
+    throw new Error(
+      `Unexpected children for link node:\n${JSON.stringify(node)}`,
+    );
+  }
+
+  const desiredIdentifier = (node.children[0] as Text).value;
+
+  let finalIdentifier;
+
+  if (!usedIdentifiers.has(desiredIdentifier)) {
+    finalIdentifier = desiredIdentifier;
+    usedIdentifiers.add(desiredIdentifier);
+  } else {
+    if (Number.parseInt(desiredIdentifier)) {
+      throw new Error(
+        `Conflicting numbered footnote identifier: ${desiredIdentifier}`,
+      );
+    }
+    let i = 2;
+    finalIdentifier = desiredIdentifier;
+    while (usedIdentifiers.has(finalIdentifier)) {
+      finalIdentifier = desiredIdentifier.repeat(i);
+      i++;
+    }
+    usedIdentifiers.add(finalIdentifier);
+  }
+
+  targetsToFinalIdentifiers.set(target, finalIdentifier);
+
+  parent.children[index] = {
+    type: "footnoteReference",
+    identifier: finalIdentifier,
+  } as FootnoteReference;
+
+  return SKIP;
+});
+
+// same for the footnotes, since footnotes contain references to other footnotes.
+// FIXME: mostly duplicated from above, turn into function
+visit(footnotes, "link", (node, index, parent) => {
+  if (!parent || typeof index !== "number") return;
+
+  const [file, target] = node.url.split("#");
+
+  // FIXME: this block is the only part that's different from the above
+  // `visit()` block over the main chapter.
+  if (file === file1) {
+    // simply remove links that return to the main file.
+    parent.children.splice(index, 1);
+    return SKIP;
+  }
+
+  if (file !== file2) {
+    throw new Error(`External link to unknown file: "${file}"`);
+  }
+  if (!target) throw new Error(`No target for link: "${node.url}"`);
+  if (node.children.length !== 1 && node.children[0].type !== "text") {
+    throw new Error(
+      `Unexpected children for link node:\n${JSON.stringify(node)}`,
+    );
+  }
+
+  const desiredIdentifier = (node.children[0] as Text).value;
+
+  let finalIdentifier;
+
+  if (!usedIdentifiers.has(desiredIdentifier)) {
+    finalIdentifier = desiredIdentifier;
+    usedIdentifiers.add(desiredIdentifier);
+  } else {
+    if (Number.parseInt(desiredIdentifier)) {
+      throw new Error(
+        `Conflicting numbered footnote identifier: ${desiredIdentifier}`,
+      );
+    }
+    let i = 2;
+    finalIdentifier = desiredIdentifier;
+    while (usedIdentifiers.has(finalIdentifier)) {
+      finalIdentifier = desiredIdentifier.repeat(i);
+      i++;
+    }
+    usedIdentifiers.add(finalIdentifier);
+  }
+
+  targetsToFinalIdentifiers.set(target, finalIdentifier);
+
+  parent.children[index] = {
+    type: "footnoteReference",
+    identifier: finalIdentifier,
+  } as FootnoteReference;
+
+  return SKIP;
+});
+
+// now walk the footnotes file and transform intermediate footnote nodes into
+// real footnote definitions using the agreed-upon identifiers.
+visit(footnotes, "html", (node, index, parent) => {
+  if (!parent || typeof index !== "number") {
+    throw new Error(`Unexpected structure:\n${JSON.stringify(node)}`);
+  }
+
+  const v = node.value;
+  if (!v.startsWith("<!-- ") || !v.endsWith(" -->")) {
+    throw new Error(`Unexpected HTML node:\n${JSON.stringify(node)}`);
+  }
+  const maybeId = v.slice(5, -4);
+
+  const identifier = targetsToFinalIdentifiers.get(maybeId);
+
+  if (!identifier) {
+    throw new Error(`No reference found to anchor "${maybeId}"!`);
+  }
+
+  parent.children[index] = {
+    type: "footnoteDefinition",
+    identifier,
+    children: [],
+  } as FootnoteDefinition;
+
+  return SKIP;
+});
+
+await writeMd(chap, file1);
+await writeMd(footnotes, file2);
+
+const md1 = toMarkdown(chap, { extensions: [gfmFootnoteToMarkdown()] });
+const md2 = toMarkdown(footnotes, { extensions: [gfmFootnoteToMarkdown()] });
+await writeFile(`parts-combined.md`, md1.concat("\n\n", md2));
+
+console.log("done.");
+
+async function loadMungeConvert(filename: string, isFootnotesFile = false) {
+  console.log("munging", filename);
+
   const resolvedFilename = join(INPUT_DIR, filename);
 
   const inFile = await readFile(resolvedFilename, "utf8");
 
   const hast = fromHtml(inFile, { fragment: true });
 
-  // this doesn't appear to get handled by hast-util-to-mdast, so we clean it up
-  // by hand.
+  // the opening namespace tag <?xml... doesn't appear to get handled by
+  // hast-util-to-mdast, so we clean it up by hand.
   if (hast.children[0].type === "comment") {
     hast.children.splice(0, 1);
   }
 
-  // collect referenced external urls
+  // retained for testing, but we no longer need the return value.
+  collectReferencedUrls(hast, isFootnotesFile);
+
+  // const footnoteIdsToElements = await collectFootnotes(urls[0]);
+
+  return convertToMdast(hast, isFootnotesFile);
+}
+
+async function writeMd(mdast: ReturnType<typeof toMdast>, filename: string) {
+  const md = toMarkdown(mdast, { extensions: [gfmFootnoteToMarkdown()] });
+  await writeFile(`${basename(filename, ".html")}.md`, md);
+}
+
+function collectReferencedUrls(hast: HastRoot, isFootnotesFile = false) {
   const referencedUrls = new Set<string>();
   visit(hast, "element", (node) => {
     if (
@@ -39,50 +214,52 @@ async function munge(filename: string) {
   });
 
   const urls = [...referencedUrls];
+  console.log("referenced urls in file:", urls.join(", "));
 
-  if (urls.length > 1) {
+  if (isFootnotesFile) {
+    if (urls.length > 2) {
+      throw new Error(
+        `More than two referenced urls found in footnotes file:\n${urls.join(", ")}`,
+      );
+    }
+  } else if (urls.length > 1) {
     throw new Error(
       `More than one referenced url found in chapter:\n${urls.join(", ")}`,
     );
   }
-  const footnoteIdsToElements = await collectFootnotes(urls[0]);
 
-  const footnoteDefinitions: FootnoteDefinition[] = [];
+  return urls;
+}
 
-  const mdast = toMdast(hast, {
+function convertToMdast(hast: HastRoot, isFootnotesFile = false) {
+  return toMdast(hast, {
     // adapted from
     // https://github.com/syntax-tree/hast-util-to-mdast/tree/52b3d5a715da233d15b711c5475d1cf40480a7cc/lib/handlers
     handlers: {
       a(state, node) {
         const properties = node.properties ?? {};
 
-        // drop links without an href
-        if (!properties.href || typeof properties.href !== "string") return;
-
-        const footnoteId = properties.href.split("#")[1];
-
-        if (footnoteId && Object.hasOwn(footnoteIdsToElements, footnoteId)) {
-          const identifier = String(footnoteDefinitions.length + 1);
-          const def = {
-            type: "footnoteDefinition",
-            identifier,
-          } as FootnoteDefinition;
-          footnoteDefinitions.push(def);
-          // state.all() call has to happen after pushing to the footnote array
-          // if we're relying on footnote count, since it may (?) generate notes
-          // of its own
-          def.children = state.all(footnoteIdsToElements[footnoteId]) as any;
-
-          const result = {
-            type: "footnoteReference",
-            identifier,
-          } as FootnoteReference;
-
-          state.patch(node, result);
-          return result;
+        // links without an href:
+        if (!properties.href || typeof properties.href !== "string") {
+          // if we're in a footnotes file and it's an anchor (ie. it has an id)
+          // convert it to a comment. we'll use that in the next ast pass.
+          if (
+            isFootnotesFile &&
+            properties.id &&
+            typeof properties.id === "string"
+          ) {
+            const result = {
+              type: "html",
+              value: `<!-- ${properties.id} -->`,
+            } as Html;
+            state.patch(node, result);
+            return result;
+          }
+          // otherwise drop it.
+          return;
         }
 
-        // regular link
+        // the rest of this is the same as the normal handler for `a` nodes.
         const children = state.all(node);
         const result = {
           type: "link",
@@ -90,97 +267,9 @@ async function munge(filename: string) {
           title: properties.title ? String(properties.title) : null,
           children,
         } as Link;
-
         state.patch(node, result);
         return result;
       },
     },
   });
-
-  (mdast as Root).children.push(...footnoteDefinitions);
-
-  const md = toMarkdown(mdast, { extensions: [gfmFootnoteToMarkdown()] });
-
-  await writeFile("cap.md", md);
-}
-
-await munge("part0014.html");
-
-console.log("done.");
-
-async function collectFootnotes(filename: string) {
-  const fnFile = await readFile(join(INPUT_DIR, filename), "utf8");
-
-  // note fragment: false -- we want to traverse the <body>
-  const fnHast = fromHtml(fnFile, { fragment: false });
-  const fnHtmlNode = fnHast.children.find(
-    (n) => n.type === "element" && n.tagName === "html",
-  ) as Element;
-  const fnBodyNode = fnHtmlNode.children.find(
-    (n) => n.type === "element" && n.tagName === "body",
-  ) as Element;
-
-  const footnoteNodes = fnBodyNode.children.map((n) => {
-    // example structure:
-    //   <p class="EB03BodyTextIndented" id="cap0008005">
-    //   <a id="cap0008006" class="calibre1"></a>
-    //   <a id="cap0008007" href="part0014.html#cap0001184" class="calibre1">
-    //     4
-    //   </a>. [[footnote contents]]
-    // </p>
-    // the <a> without href is the footnote id.
-    if (n.type !== "element" || n.tagName !== "p") return null;
-
-    const i = n.children.findIndex((fc) => fc.type === "element");
-
-    if (i === -1) return null;
-
-    const firstChild = n.children[i] as Element;
-
-    if (
-      !firstChild ||
-      firstChild.tagName !== "a" ||
-      firstChild.properties.href !== undefined ||
-      typeof firstChild.properties.id !== "string" ||
-      !firstChild.properties.id
-    ) {
-      return null;
-    }
-
-    const identifier = firstChild.properties.id;
-
-    const newNode = { ...n };
-
-    // remove whitespace
-    const children = newNode.children
-      .filter((c) => c.type !== "text" || c.value.trim().length > 0)
-      .map((c) => {
-        if (c.type !== "text") return c;
-
-        let value = c.value.trim();
-        if (value.startsWith(".")) {
-          value = value.slice(1);
-        }
-
-        return {
-          ...c,
-          value: value
-            .split("\n")
-            .map((l) => l.trim())
-            .join(" "),
-        };
-      });
-    newNode.children = children;
-
-    // FIXME: this is wrong and fragile, happens to work because footnotes all
-    // open with whitespace nodes that are filtered in the previous step
-    children.splice(i, 1);
-
-    return [identifier, newNode] as const;
-  });
-
-  const filtered = footnoteNodes.filter((n) => n) as [string, Element][];
-  const footnoteIdsToElements = Object.fromEntries(filtered);
-
-  return footnoteIdsToElements;
 }
